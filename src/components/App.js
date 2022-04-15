0f62d76db06284f8c81079e6b91306d075cd182d
Manav Shah <76565191+themanavshah@users.noreply.github.com>
2022-04-15 10:52:38 +0530

    react not working

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+/* eslint-disable react/react-in-jsx-scope */
+import {Component} from "react";
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>NFT marketplace</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
